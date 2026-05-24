00db76512d0d18bcc344b7d675a5ff09c358d26e
niko5443 <184934855+Nika221-bit@users.noreply.github.com>
2026-05-25 00:07:09 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playRandomNum;
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to Roll the dice");
+
+                Console.ReadKey();
+                playRandomNum = random.Next(1, 7);
+                Console.WriteLine("you rolled a" + " " + playRandomNum);
+            }
+
+            Console.ReadKey();
+        }
+    }
+  
+}
