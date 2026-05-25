297bd48772fa60790d26edcb9bf20679750e3288
niko5443 <184934855+Nika221-bit@users.noreply.github.com>
2026-05-25 15:48:59 +0400

    done

--- Program.cs
+++ Program.cs
@@ -13,28 +13,45 @@ namespace simple_Dice_Game
             int playRandomNum;
             int enemyRandomNum;
 
+            int playerScore = 0;
+            int enemyScore = 0;
+
             Random random = new Random();
 
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Press any key to Roll the dice");
 
-                Console.ReadKey();
+               
                 playRandomNum = random.Next(1, 7);
                 Console.WriteLine("you rolled a" + " " + playRandomNum);
 
                 enemyRandomNum = random.Next(1, 7);
                 Console.WriteLine("Enemy rolled a" + " " + enemyRandomNum);
 
+                
+
+                if (playRandomNum > enemyRandomNum)
+                {
+                    playerScore++;
+                    Console.WriteLine("Player win this round!");
+                }
+                else if (playRandomNum < enemyRandomNum)
+                {
+                    Console.WriteLine("Enemy win this round!");
+                }
+                else
+                {
+                    Console.WriteLine("It's a tie!");
+
+
+                }
                 Console.WriteLine("...");
                 System.Threading.Thread.Sleep(1000);
-               
-              
-            }
-            
 
-            Console.ReadKey();
+                Console.ReadKey();
+            }
         }
+
     }
-  
 }
